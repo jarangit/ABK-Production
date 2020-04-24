19803739f9d5ef81f750c9523916c6c0b4d8c106
jarangit <jaran.dch@gmail.com>
2020-04-24 16:24:17 +0700

    create global style

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,9 +1,44 @@
 import React from "react"
 import MainMenu from './memus'
 import Footer from './footer'
-import './Layout.css'
+// import './Layout.css'
 import '../../lib/fontawesome/css/all.css'
-import styled from 'styled-components'
+import {styled, createGlobalStyle} from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+  body,html{
+    padding: 0 ;
+    margin: 0 ; 
+    height: 100%; 
+  }
+  body{
+    display: flex;
+    flex-direction: column;
+  }
+@font-face {
+  font-family: myFirstFont;
+  src: url(../font/Kanit-Light.ttf);
+}
+
+* {
+  font-family: myFirstFont;
+}
+
+h1 ,h2 ,h3 ,h4 ,h5 ,h6,p {
+  padding: 0;
+  margin: 15px 0;
+}
+a {
+  text-decoration: none;
+  color: #17949b;
+}
+
+.toggle {
+  text-align: right;
+  display:none;
+} 
+`
 
 const LayoutWrapper = styled.div`
     max-width:1064px;
@@ -16,13 +51,13 @@ const LayoutWrapper = styled.div`
 
 const Layout = ({ children }) => {
   return(
-    <div>
+    <GlobalStyle>
       <MainMenu/>
         <LayoutWrapper>
             {children}
         </LayoutWrapper>
       <Footer/>
-    </div>
+    </GlobalStyle>
   )
 }
 

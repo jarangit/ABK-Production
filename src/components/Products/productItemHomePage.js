7f1f6bb062c40000bfc930548cba4942cfd1c963
jarangit <jaran.dch@gmail.com>
2020-05-01 08:05:23 +0700

    23

--- src/components/Products/productItemHomePage.js
+++ src/components/Products/productItemHomePage.js
@@ -64,7 +64,7 @@ const ProductItemHomePage = (props) => {
                     <div> {name} </div>
                     <div> {regularPrice} </div>
                     <Link key={id} to = {url}>
-                        <button> รายละเอียดสินค้า test </button>
+                        <button> รายละเอียดสินค้า </button>
                     </Link>
         </Block>
     )

308e33bc72595844769b9ba2f80addcddfd7cdd4
jarangit <jaran.dch@gmail.com>
2020-05-01 08:13:01 +0700

    123

--- src/components/Products/productListHomePage.js
+++ src/components/Products/productListHomePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import styled from 'styled-components'
-import ProductItemHomePage from './ProductItemHomePage'
+import ProductItemHomePage from './productItemHomePage'
 
 
 

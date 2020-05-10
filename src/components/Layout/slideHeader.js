cbd0b5db8b7bea4501ce16d647f4c45f406d0343
jarangit <jaran.dch@gmail.com>
2020-05-11 03:40:20 +0700

    fix banner

--- src/components/Layout/slideHeader.js
+++ src/components/Layout/slideHeader.js
@@ -14,24 +14,23 @@ import { globalHistory } from "@reach/router"
 
 //------------------------------------------------------------
 const SlideHeader = (props) => {
-   const pathName = globalHistory.location.pathname
+    const pathName = globalHistory.location.pathname
     const {dataImgToSlide} = props
-    console.log(dataImgToSlide)
-    console.log(pathName)
-    if(pathName === '/'){
+
+    if(dataImgToSlide != undefined){
+      return(
+        <div>
+          <img src= {props.dataImgToSlide} width = "100%" />
+        </div>
+      )
+    }else if(pathName === '/'){
       return(
         <div>
           <img src= "https://api.abk-store.com/wp-content/uploads/2020/05/welding-2178127.png" width = "100%" />
         </div>
       )
-    }else if(dataImgToSlide === undefined){
-      return 'no banner'
     }else{
-      return(
-        <div>
-        <img src= {props.dataImgToSlide} width = "100%" />
-      </div>
-      )
+      return 'no banner'
     }
     
 }

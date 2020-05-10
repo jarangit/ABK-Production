3e0bfb585b33c445504eb6d5184f6b2d9b76ceb2
jarangit <jaran.dch@gmail.com>
2020-05-11 03:24:17 +0700

    fix bannner

--- src/components/Layout/slideHeader.js
+++ src/components/Layout/slideHeader.js
@@ -22,7 +22,7 @@ const SlideHeader = (props) => {
     }else if(pathName === '/'){
       return(
         <div>
-          <img src= "https://api.abk-store.com/wp-content/uploads/2020/05/welding-2178127.png" width = "100%" />
+          {/* <img src= "https://api.abk-store.com/wp-content/uploads/2020/05/welding-2178127.png" width = "100%" /> */}
         </div>
       )
     }else{

b50f32c0329964a5e1e155676c1264f0b82e77f1
jarangit <jaran.dch@gmail.com>
2020-05-14 22:21:42 +0700

    chenge sixe img icon to styled component

--- src/components/Modal/ModalOncClickSale.js
+++ src/components/Modal/ModalOncClickSale.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from 'styled-components'
 
-
+const Img = styled.img`
+        margin: 5px 10px;
+        width:50px;
+`
 const ModalSty = styled.div`
     position: fixed; /* Stay in place */
     z-index: 1; /* Sit on top */
@@ -36,10 +39,6 @@ const ModalSty = styled.div`
                 list-style:none;
                 display:flex;
                 /* display:inline-block; */
-                img{
-                    margin: 5px 10px;
-                    width:50px;
-                }
                 a{
                     list-style:none;
                     display:flex;
@@ -74,25 +73,25 @@ const ModalOnClickSale = (props) => {
                         <ul>
                             <li>
                                 <a href = "https://line.me/R/ti/p/%40426ovedm" target="_blank" >
-                                <img src="https://api.abk-store.com/wp-content/uploads/2020/05/f7e8c32d420b6da05f807488ebf8a1e5.png" />
+                                <Img src="https://api.abk-store.com/wp-content/uploads/2020/05/f7e8c32d420b6da05f807488ebf8a1e5.png" />
                                 <p>Line</p>
                                 </a>
                             </li>
                             <li>
                                 <a href = "https://www.facebook.com/Jaranservice" target="_blank">
-                                <img src="https://api.abk-store.com/wp-content/uploads/2020/05/Facebook_Logo_2019.png" />
+                                <Img src="https://api.abk-store.com/wp-content/uploads/2020/05/Facebook_Logo_2019.png" />
                                 <p>Facebook</p>
                                </a>
                             </li>
                             <li>
                                 <a href="mailto: abk.contactus@gmail.com" >
-                                <img src="https://api.abk-store.com/wp-content/uploads/2020/05/mail.png" />
+                                <Img src="https://api.abk-store.com/wp-content/uploads/2020/05/mail.png" />
                                 <p>abk.contactus@gmail.com</p>
                                 </a>
                             </li>
                             <li>
                                 <a href ="tel:0895937868" >
-                                <img src="https://api.abk-store.com/wp-content/uploads/2020/05/79dbd38bce56b49fdc7a04d3b01dee41.png" />
+                                <Img src="https://api.abk-store.com/wp-content/uploads/2020/05/79dbd38bce56b49fdc7a04d3b01dee41.png" />
                                 <p>08-959-37868</p>
                                 </a>
                             </li>

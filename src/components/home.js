a5505f90825e1ddb98097bd93cf4636fab6bcc1f
jarangit <jaran.dch@gmail.com>
2020-05-25 17:44:38 +0700

    fix seo titel

--- src/components/home.js
+++ src/components/home.js
@@ -3,13 +3,11 @@ import JustWeb from './HomePage/just-web';
 import ProductListHomePage from './Products/productListHomePage'
 import BlogListHomepage from './ฺBlog/blogListHomePage'
 import Promote from './HomePage/promote';
-import SEO from './seo'
 
 
 const HomePage = () =>  {
         return(
            <div>
-                        <SEO title = 'ABK'/>
                         <JustWeb/>
                         <ProductListHomePage/>
                         <Promote/>
